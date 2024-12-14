14df3868d195e24e923bcb20aa8bf8beab5511fd
MuhammadSofi <sofimuhammad25@gmail.com>
2024-12-15 02:50:10 +0700

    update code (refer to next js docs) https://nextjs.org/docs/messages/sync-dynamic-apis
    fixing issue Route "/api/posts/[id]" used 'params.id'. 'params' should be awaited before using its properties.

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -2,8 +2,39 @@ import prisma from "../../../../../prisma/client";
 
 import { formatDate, responseOut } from "../../../helper/helper";
 
-export async function GET(request, { params }) {
+// export async function GET(request, { params }) {
+//     const id = parseInt(params.id);
+
+//     const post = await prisma.post.findUnique({
+//         select: {
+//             title: true,
+//             content: true,
+//             createdAt: true
+//         },
+//         where: {
+//             id,
+//         },
+//     });
+
+//     if(!post) {
+//         return responseOut(false, 404, "Detail Data Post Not Found", null);
+//     }
+
+//     // manipulate response
+//     post.createdAt = formatDate(post.createdAt);
+
+//     return responseOut(true, 200, "Detail Data Post", post);
+// }
+
+// improvement from next js 15
+export async function GET(request, context) {
+    const params = await context.params; // Await params explicitly here
     const id = parseInt(params.id);
+
+    // checking if input is not a
+    if (isNaN(id)) {
+        return responseOut(false, 400, "Invalid ID", null);
+    }
 
     const post = await prisma.post.findUnique({
         select: {
