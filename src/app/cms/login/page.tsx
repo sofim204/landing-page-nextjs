18d5e6c9c4d59437acb8068d10a7cfc4dd719cdb
sofim204 <sofimuhammad25@gmail.com>
2024-12-16 13:19:02 +0700

    test separate page(for cms)

---
+++ src/app/cms/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <div>
+                <p>this is login page</p>
+            </div>
+        </>
+    );
+}
---
+++ src/app/cms/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() { 
+    return (
+        <>
+            <div>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum accusamus tenetur quia. A dolore corporis quae rerum. Error, adipisci aut.</p>
+            </div>
+        </>
+    );
+}
